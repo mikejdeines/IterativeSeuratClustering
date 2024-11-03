74a318a07bcb9855fafd2aa8f8cd9e66af2ec07d
mikejdeines <deines@purdue.edu>
2024-11-03 12:30:41 -0500

    Add files via upload

--- R/iterative_clustering.R
+++ R/iterative_clustering.R
@@ -102,8 +102,8 @@ clustering_score <- function(seurat_object, pct.1){
   #' @returns a clustering score
   require(Seurat)
   de.genes <- FindMarkers(seurat_object, ident.1 = 1, ident.2 = 2, logfc.threshold = 1, min.pct = pct.1, recorrect_umi = FALSE)
-  de.genes <- subset(de.genes, abs(de.genes$avg_log2FC) > 2)
   de.genes[de.genes$p_val_adj == 0,5] <- 1e-20
+  de.genes <- subset(de.genes, abs(de.genes$avg_log2FC) > 2)
   score <- -log10(de.genes$p_val_adj)
   score[score > 20] <- 20
   score <- sum(score)
